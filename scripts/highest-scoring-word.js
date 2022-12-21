666d911fde31c38587fcce12d15decb9d919e4d4
Alexey Belkov <lexabelco2013@yandex.ru>
2022-12-21 23:18:38 +0500

    highest-scoring-word, string-incrementer, kata-example-twist

---
+++ scripts/highest-scoring-word.js
@@ -0,0 +1,33 @@
+/* 
+https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/javascript
+
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid. */
+
+function high(x){
+  x = x.split(' ');
+  let res = '';
+  let sumFirst = 0;
+  
+  x.forEach((word) => {
+    let sum = 0; 
+
+    for(let i = 0; i < word.length; i++) {
+      sum += word.charCodeAt(i) - 96;
+    }
+    
+    if(sum > sumFirst) {
+        res = word;
+        sumFirst = sum;
+    }
+  });
+  
+  return res;
+}
---
+++ scripts/kata-example-twist.js
@@ -0,0 +1,13 @@
+/*
+https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/javascript
+
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+*/
+
+var websites = []
+
+for(let i = 0; i < 1000; i++) {
+  websites.push('codewars');
+}
