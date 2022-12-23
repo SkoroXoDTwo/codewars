2c3a471d253d3b4fdd6ef79237c2129210629213
Alexey Belkov <lexabelco2013@yandex.ru>
2022-12-23 16:08:01 +0500

    alan-partridgeII-apple-turnover, odd-even-string-sort, clocky-mc-Clock-face

---
+++ scripts/alan-partridgeII-apple-turnover.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
+
+As a treat, I'll let you read part of the script from a classic 'I'm Alan Partridge episode:
+Alan is known for referring to the temperature of the apple turnover as Hotter than the sun!. According to space.com the temperature of the sun's corona is 2,000,000 degrees Celsius, but we will ignore the science for now.
+
+Task
+Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+Note: Input will either be a positive integer (or a string for untyped languages).
+
+*/
+
+function apple(x){
+  return Number(x) ** 2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
